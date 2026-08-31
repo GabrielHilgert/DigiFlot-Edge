62129638dd9b1320f3d0c12a98bdafca79d62ae8
Gabriel R. Hilgert <g.hilgert@hzdr.de>
2026-08-31 17:49:34 +0200

    Update on Launch

--- src/launch.sh
+++ src/launch.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
+git fetch origin
+
+if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
+    git pull --ff-only origin main
+    sudo systemctl restart digiflot
+fi
 
 source .venv/bin/activate
 exec uvicorn app:app --host 0.0.0.0 --port 8000 --workers 1
